ef4f606089756897a7bd61d7161921f5aa7f752a
aktsbot <box.ashishk@gmail.com>
2019-08-13 23:37:10 +0530

    I got ipv6 with my jio connection. Crap!

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # i - client info 
 
 I got tired of visiting https://whatismyipaddress.com/ and other sites that
-bombard me with ads when I just want my public IP.
+bombard me with ads when I just want my **public ip**.
+
+## Setup 
+
+```
+$ git clone https://github.com/aktsbot/i
+$ cd i
+$ npm install
+```
+
+## Run
+
+```
+$ PORT=1337 node server.js
+```
 
 **Live:** 
 
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 // https://blog.risingstack.com/your-first-node-js-http-server/
+// https://github.com/CITGuru/express-ip/blob/master/index.js
 
 const http = require('http');
+const geoip = require('geoip-lite');
+
 const port = process.env.PORT || 3000;
+
+const getIpInfo = (ip) => {
+  // IPV6 addresses can include IPV4 addresses
+  // So req.ip can be '::ffff:86.3.182.58'
+  // However geoip-lite returns null for these
+  if (ip.includes('::ffff:')) {
+    ip = ip.split(':').reverse()[0]
+  }
+  const lookedUpIP = geoip.lookup(ip);
+  if ((ip === '127.0.0.1' || ip === '::1')) {
+    return "THIS IS LOCALHOST!";
+  }
+  if (!lookedUpIP) {
+    return "geoip-lite b0rked";
+  }
+  return lookedUpIP;
+}
 
 const requestHandler = (req, res) => {
   console.log(req.url);
@@ -9,7 +29,8 @@ const requestHandler = (req, res) => {
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  res.end(ip);
+  const ipInfo = getIpInfo(ip);
+  res.end(ipInfo);
 }
 
 const server = http.createServer(requestHandler)
