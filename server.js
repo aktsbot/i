dd680e78c61572fd4a63397f6f12448b73b76b50
aktsbot <box.ashishk@gmail.com>
2019-08-14 00:26:45 +0530

    trust proxies

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+// https://expressjs.com/en/guide/behind-proxies.html
+app.enable('trust proxy');
 
 app.get('/', (req, res) => {
   console.log('-------')
