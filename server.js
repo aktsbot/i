d7b2ef57b2dff2900bc63b7199540b2bb851a3d1
aktsbot <box.ashishk@gmail.com>
2019-08-14 00:14:07 +0530

    ipv4 not happening

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
 // https://blog.risingstack.com/your-first-node-js-http-server/
-// https://github.com/CITGuru/express-ip/blob/master/index.js
 
 const http = require('http');
-const geoip = require('geoip-lite');
 
 const port = process.env.PORT || 3000;
 
-const getIpInfo = (ip) => {
-  // IPV6 addresses can include IPV4 addresses
-  // So req.ip can be '::ffff:86.3.182.58'
-  // However geoip-lite returns null for these
-  if (ip.includes('::ffff:')) {
-    ip = ip.split(':').reverse()[0]
-  }
-  const lookedUpIP = geoip.lookup(ip);
-  if ((ip === '127.0.0.1' || ip === '::1')) {
-    return "THIS IS LOCALHOST!";
-  }
-  if (!lookedUpIP) {
-    return "geoip-lite b0rked";
-  }
-  return lookedUpIP;
-}
-
 const requestHandler = (req, res) => {
   console.log(req.url);
+  const realIp = req.headers["x-real-ip"];
   const ip = req.headers['x-forwarded-for'] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  const ipInfo = getIpInfo(ip);
-  res.end(ipInfo);
+  const ips = `${realIp}
+${ip}
+  `
+  res.end(ips);
 }
 
 const server = http.createServer(requestHandler)
